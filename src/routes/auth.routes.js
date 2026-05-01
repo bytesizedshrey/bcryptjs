37c5491218f1dad7853bed19b15611f93b3a8c20
bytesizedshrey <shreyash18gajbhiye@icloud.com>
2026-05-01 15:03:53 +0530

    login route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 const authRouter = express.Router() //defined a router
 
 //Store in Database
+// controller
 authRouter.post("/register",async(req,res)=>{
     const {email,name,password} = req.body //Extract the Data
 
@@ -39,12 +40,49 @@ authRouter.post("/register",async(req,res)=>{
     })
 })
 
+//Protected Route
+// Check if cookie exists (e.g., token)
+// Verify it (JWT / session)
+// Allow or reject
 authRouter.post("/protected",async(req,res)=>{
     console.log(req.cookies)
 
     res.status(200).json({
         message : "this is a protected route"
     })
+})
+
+//login route -> if right then generate a new token and give it to the user as a cookie
+authRouter.post("/login", async (req,res)=>{
+    const{email,password} = req.body
+
+    //check if the user even exists or not
+    const user = await userModel.findOne({email})
+
+    if(!user){
+        return res.status(404).json({
+            message : "User not found with this email address."
+        })
+    }
+    //if the email exists then check if the password is correct not not
+    const isPasswordMatched = user.password === password
+    if(!isPasswordMatched){
+        return res.status(401).json({
+            message : "Invalid Password. Please try again."
+        })
+    }
+    //if password is correct then generate a new token and give it to the user as a cookie
+     const token = jwt.sign({
+        id: user._id,
+     }, process.env.JWT_SECRET)
+
+     res.cookie("jwt_token", token)
+
+     res.status(200).json({
+        message : "Login Successful",
+        user,
+     })
+
 })
 
 module.exports = authRouter //Export Router
